3d4222401fafe0bde786784a041b40b6ee3ee081
Shivani Gupta <shivani.gupta.28.2@mountblue.tech>
2024-07-17 12:41:07 +0530

    loader

--- src/components/utilityComponents/Loader.jsx
+++ src/components/utilityComponents/Loader.jsx
@@ -7,7 +7,7 @@ function Loader() {
     const loader = useSelector(selectLoader);
 
     return (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-white opacity-100">
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-white">
             {loader && (
                 <BallTriangle
                     height={100}
